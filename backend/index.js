fcb24b35df4755aaad08036fb60f8014ddcb03dd
FlachbandkabelPeter <61627082+FlachbandkabelPeter@users.noreply.github.com>
2024-10-30 20:04:34 +0100

    #31  Backend setup
    Basic installation of Express JS and Node JS + Example for the implementation of an MVC structure (without V)

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Routen importieren
+const exampleRoutes = require('./routes/example');
+
+// Middleware (optional)
+const exampleMiddleware = require('./middlewares/exampleLogger');
+app.use(exampleMiddleware);
+
+//Routen verwenden
+app.use('/', exampleRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
