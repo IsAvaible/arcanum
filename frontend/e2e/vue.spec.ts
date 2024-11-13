fc4e6f96433e9c3cce2c95d5a90b49962be1ab4c
Alex <alex.djomo@alumni.fh-aachen.de>
2024-11-13 03:00:49 +0100

    update ci.yml

--- frontend/e2e/vue.spec.ts
+++ frontend/e2e/vue.spec.ts
@@ -4,5 +4,10 @@ import { test, expect } from '@playwright/test';
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+
+  // Warten Sie, bis das Element vorhanden ist, bevor der Text geprüft wird.
+  await page.waitForSelector('div.greetings > h1', { timeout: 10000 }); // 10 Sekunden Timeout
+
+  // Überprüfen Sie den Text des Elements.
+  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!', { timeout: 10000 });
+});
