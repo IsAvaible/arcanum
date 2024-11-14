72726c3ae446f65acfb3a0afe15a088ed2dbf659
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2024-11-14 01:06:44 +0100

    fix(ci): fix playwright base url and adjust gitignore

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -28,13 +28,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.CI ? 'http://localhost:4173' : 'http://localhost:5173',
+    baseURL: process.env.CI ? 'http://localhost:5173' : 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -97,7 +97,7 @@ export default defineConfig({
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'playwright-report/artifacts',  // screenshot output directory Specify
+  outputDir: 'playwright-artifacts',  // screenshot output directory Specify
 
   /* Run your local dev server before starting the tests */
   webServer: {
