afaf53c2ebce9b707b5c01d9d0acd7f0b08885d7
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2024-11-14 10:20:21 +0100

    fix(ci): change artifact paths

--- frontend/e2e/vue.spec.ts
+++ frontend/e2e/vue.spec.ts
@@ -4,6 +4,5 @@ import { test, expect } from '@playwright/test';
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  await page.waitForLoadState('networkidle');
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
 })
