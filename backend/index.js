095d836cfade1347aad3ce0c0c8406c46817e0d1
maxGCode <max.gerdes@alumni.fh-aachen.de>
2024-12-01 20:28:49 +0100

    feature: added functionality to enforce https an created demo certificates

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
+const https = require('https');
+const hostname =  "192.168.0.114";
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 443;
 const caseRoutes = require('./routes/caseRoutes');
 const chatBotRoutes = require('./routes/chatBotRoutes');
 const uploadRoutes = require('./routes/exampleFileUpload');
@@ -12,6 +15,7 @@ app.set('view engine', 'ejs');
 
 // Middleware (optional)
 const exampleMiddleware = require('./middlewares/exampleLogger');
+const { DEFAULT_MIN_VERSION } = require('tls');
 app.use(exampleMiddleware);
 
 
@@ -29,6 +33,24 @@ app.use('/', uploadRoutes);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+const options = {
+  key: fs.readFileSync('./certs/localhost-dev-key.pem'),
+  cert: fs.readFileSync('./certs/localhost.pem'),
+  //ca: fs.readFileSync('./certs/ca.pem')
+};
+
+/* try{
+  const server = https.createServer(options, app).listen(port, function(req, res){
+
+  console.log(`Example app listening on port ${port}`)
+});
+}catch(err){
+  console.error(err);
+}
+ */
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
