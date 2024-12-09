538962bb25699dc0ba2d70f0f762359c85780a0d
MarioOpatz <mario.opatz@alumni.fh-aachen.de>
2024-12-09 18:47:04 +0100

    chore(ci): added linter file

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,12 +1,7 @@
 import globals from "globals";
 
-<<<<<<< HEAD
-/** @type {import('eslint').Linter.Config[]} */
-export default [{ languageOptions: { globals: globals.node } }];
-=======
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {languageOptions: { globals: globals.browser }},
 ];
->>>>>>> c1c12f5 (chore(ci): added linter file)
