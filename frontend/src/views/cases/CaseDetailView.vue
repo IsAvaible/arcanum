86101ed6282a700801d83f4694d6b4acffed3e67
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2024-12-14 11:25:57 +0100

    refactor(validation): infer types directly from the schema

--- frontend/src/views/cases/CaseDetailView.vue
+++ frontend/src/views/cases/CaseDetailView.vue
@@ -594,7 +594,7 @@ const toggleMenu = (event: Event) => {
                 <div v-if="!loading">
                   <UserSelector
                     @update:selected-users="
-                      (fields.assignees.value.value = $event.map((u) => u.name))
+                      fields.assignees.value.value = $event.map((u) => u.name)
                     "
                     assigneeLabel="Assignees"
                     :placeholder="inEditMode ? 'Select Assignees' : ''"
