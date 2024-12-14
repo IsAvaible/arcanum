ed6547c8c00f2d8455df69cb5efa5a6d44eb169a
TC <timo.ug@hotmail.com>
2024-12-14 12:36:55 +0100

    Cut Video Segments

--- llm_backend/video.py
+++ llm_backend/video.py
@@ -6,11 +6,15 @@ from app import app
 import cv2
 
 
-def cut_video_segments(input_file, output_dir, segment_duration=120):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def cut_video_segments(input_file, filehash, segment_duration=100):
 
-    output_pattern = os.path.join(output_dir, "segment_%03d.mp4")
+    output_path = os.path.join(
+        app.root_path, os.path.join("temp/video_segments", f"{filehash}_video")
+    )
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    output_pattern = os.path.join(output_path, f"videos_%04d.mp4")
 
     command = [
         "ffmpeg",
@@ -25,65 +29,79 @@ def cut_video_segments(input_file, output_dir, segment_duration=120):
 
     try:
         subprocess.run(command, check=True)
-        print(f"Video successfully split into segments. Saved in {output_dir}")
+        return output_path
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while splitting the video: {e}")
 
 def extract_frames_with_ffmpeg(video_path, filehash):
     # Erstelle den Ausgabeordner, falls er nicht existiert
+    single_video = video_path
+
     path = os.path.join(
-        app.root_path, os.path.join("temp", f"{filehash}_frames")
+        app.root_path, os.path.join("temp/frames", f"{filehash}_frames")
     )
     if not os.path.exists(path):
         os.makedirs(path)
     # Berechne die Skalierung
-    cam = cv2.VideoCapture(video_path)
+    cam = cv2.VideoCapture(single_video)
     fps = cam.get(cv2.CAP_PROP_FPS)
     total_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
     duration = total_frames / fps  # Dauer des Videos in Sekunden
 
-    # Maximal 50 Frames extrahieren
-    max_frames = 50
-    frame_interval = duration / max_frames  # Intervall zwischen Frames in Sekunden
+    if(duration > 120):
+        segments_path = cut_video_segments(single_video, filehash)
+    else:
+        segments_path = [single_video]
 
-    vf_filter = f"fps=1/{frame_interval} ,scale=320:-1"
+    for video in segments_path:
+        # Maximal 50 Frames extrahieren
+        max_frames = 50
+        frame_interval = duration / max_frames  # Intervall zwischen Frames in Sekunden
 
-    # FFmpeg-Befehl ausführen
-    output_pattern = os.path.join(path, f"frame_%04d.jpg")
-    command = [
-        "ffmpeg",
-        "-y",
-        "-i", video_path,
-        "-vf", vf_filter,
-        output_pattern
-    ]
-    try:
-        subprocess.run(command, check=True)
-        print(f"Bilder erfolgreich in {path} gespeichert.")
-    except subprocess.CalledProcessError as e:
-        print(f"Fehler beim Ausführen von FFmpeg: {e}")
+        vf_filter = f"fps=1/{frame_interval} ,scale=320:-1"
+
+        # FFmpeg-Befehl ausführen
+        output_pattern = os.path.join(path, f"frame_%04d.jpg")
+        command = [
+            "ffmpeg",
+            "-y",
+            "-i", video,
+            "-vf", vf_filter,
+            output_pattern
+        ]
+        try:
+            subprocess.run(command, check=True)
+            print(f"Saved frames: {output_pattern} ")
+        except subprocess.CalledProcessError as e:
+            print(f"Error FFMPEG (Frame Extraction): {e}")
 
 
     output = os.path.join(path, f"audio.mp3")
     command2 = [
         "ffmpeg",
         "-y",
-        "-i", video_path,
+        "-i", single_video,
         "-vn",
         output
     ]
 
     try:
         subprocess.run(command2, check=True)
-        print(f"Audio erfolgreich in {path} gespeichert.")
+        print(f"Saved Audio in {output}")
     except subprocess.CalledProcessError as e:
-        print(f"Fehler beim Ausführen von FFmpeg: {e}")
-
+        print(f"Error FFMPEG (Audio Extraction): {e}")
 
     return path
 
 
 def get_all_frames_in_dir(path):
+    f = []
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for file in filenames:
+            f.append(os.path.join(dirpath, file))
+    return f
+
+def get_all_video_segments_in_dir(path):
     f = []
     for (dirpath, dirnames, filenames) in os.walk(path):
         for file in filenames:
