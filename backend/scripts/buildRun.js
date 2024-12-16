6a291f49e6a20fa986991ecf5e2ac1f7395e7ac6
MarioOpatz <mario.opatz@alumni.fh-aachen.de>
2024-12-16 17:26:29 +0100

    feat(docker): adapt server to support dynamic port configuration for Docker

--- backend/scripts/buildRun.js
+++ backend/scripts/buildRun.js
@@ -4,21 +4,20 @@ require("dotenv").config();
 try {
   const app = express();
 
-  if (!process.env.PORT) {
-    throw new Error("Missing required environment variable: PORT");
-  }
-  console.log("Environment variables loaded successfully.");
+  // Dynamisch den Port setzen, standardmäßig auf 443 (Container-Port)
+  const PORT = process.env.PORT || 3000;
+
+  console.log(`Using port: ${PORT}`);
 
   app.use(require("body-parser").json());
   app.use(require("cors")());
   console.log("Middleware loaded successfully.");
 
-  const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 
   console.log("Application initialized without errors.");
-  process.exit(0);
 } catch (err) {
   console.error("Error during application initialization:", err.message);
   process.exit(1);
