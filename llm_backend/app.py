45904808393aaefe49bc0118cc41808e81bd3ea4
TC <timo.ug@hotmail.com>
2024-12-17 11:51:52 +0100

    Clean Up

--- llm_backend/app.py
+++ llm_backend/app.py
@@ -1,8 +1,8 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
-import os
-
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +10,6 @@ app.secret_key = "super secret key"
 app.config["SECRET_KEY"] = "super secret key"
 socketio = SocketIO(app)
 app.config["UPLOAD_FOLDER"] = "upload"
-
 
 upload_folder = os.path.join(app.root_path, "upload")
 temp_folder = os.path.join(app.root_path, "temp")
