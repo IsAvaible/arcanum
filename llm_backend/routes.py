d956085b7c3cde46fd2828f54f719236165cc53e
TC <timo.ug@hotmail.com>
2024-12-17 11:05:33 +0100

    Changed method name

--- llm_backend/routes.py
+++ llm_backend/routes.py
@@ -2,7 +2,7 @@ from flask import request, Blueprint
 from app import app
 
 from generate import (
-    generate_case_langchain_production,
+    generate,
 )
 from chat import chat
 
@@ -12,7 +12,7 @@ routes = Blueprint("routes", __name__)
 @app.route("/generate_case", methods=["POST"])
 def generate_case():
     if request.method == "POST":
-        return generate_case_langchain_production(request)
+        return generate(request)
 
 
 @app.route("/chat", methods=["POST"])
