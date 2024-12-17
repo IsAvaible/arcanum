adb3e1ff15c1e1be1be1b5457aff92c50cfa7b38
TC <timo.ug@hotmail.com>
2024-12-17 11:54:06 +0100

    Changed Download Cache Method

--- llm_backend/webdav.py
+++ llm_backend/webdav.py
@@ -1,8 +1,10 @@
 import os
 
 from dotenv import load_dotenv
-from app import app
 from webdav3.client import Client
+
+from app import app
+
 load_dotenv()
 
 options = {
@@ -36,17 +38,23 @@ def download_folder_webdav(filepath):
 
 
 def upload_cache_file(file_path, hash):
-    client.upload_sync(remote_path=f"/IP_WKS/LLM_CACHE/{hash}.json", local_path=file_path)
+    client.upload_sync(remote_path=f"/IP_WKS/LLM_CACHE/{hash}", local_path=file_path)
+
 
 def check_if_cached(hash):
-    return client.check(f"/IP_WKS/LLM_CACHE/{hash}.json")
+    return client.check(f"/IP_WKS/LLM_CACHE/{hash}")
+
 
 def download_cache(hash):
-    path = os.path.join(
-        app.root_path, os.path.join("temp", hash)
+    download_path = os.path.join(
+        app.root_path, os.path.join(f"temp/", f"{hash}")
     )
-    upload_path = os.path.join(app.root_path, os.path.join("temp"))
-    if not os.path.exists(upload_path):
-        os.makedirs(upload_path)
-    client.download_sync(remote_path=f"/IP_WKS/LLM_CACHE/{hash}.json", local_path=path)
-    return path
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    download_file = os.path.join(
+        app.root_path, os.path.join(download_path, "cache.json")
+    )
+    remote_path = f"/IP_WKS/LLM_CACHE/{hash}"
+    client.download_sync(remote_path=remote_path, local_path=download_file)
+    return download_file
