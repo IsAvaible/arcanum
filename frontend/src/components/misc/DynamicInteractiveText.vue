6fbf5838635cc0fbbdbeceec289cf02f18e96c72
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2024-12-18 09:49:25 +0100

    feat(frontend): add click event and none-link match capability to DynamicInteractiveText.vue

--- frontend/src/components/misc/DynamicInteractiveText.vue
+++ frontend/src/components/misc/DynamicInteractiveText.vue
@@ -2,13 +2,19 @@
   <span>
     <template v-for="(part, index) in parsedText" :key="index">
       <router-link
-        v-if="part.isLink"
-        :to="`${to}${part.linkId!}`"
+        v-if="part.matched && to !== undefined"
+        :to="`${to}${part.match!}`"
         :target="target"
+        @click="emit('match-click', $event, part.match!)"
         class="hover:underline"
       >
         {{ part.text }}
       </router-link>
+      <!-- Case 2: The part was matched but no to prop was provided -->
+      <button v-else-if="part.matched" @click="emit('match-click', $event, part.match!)">
+        {{ part.text }}
+      </button>
+      <!-- Case 3: The part was not matched -->
       <span v-else>{{ part.text }}</span>
     </template>
   </span>
@@ -27,30 +33,35 @@ interface Props {
    */
   text: string
   /**
-   * The regex to match the linkId, the first group should be the linkId
+   * The regex to match, the first group should be the match
    */
   regex: RegExp
   /**
-   * The base path to append the linkId to
+   * The base path to append the match to
    */
-  to: string
+  to?: string
   /**
    * The target to open the link in
    */
   target?: '_self' | '_blank' | '_parent' | '_top' | '_unfencedTop'
   /**
-   * The validator function to check if the linkId is valid
+   * The validator function to check if the match is valid
    */
-  validate?: (linkId: string) => Promise<boolean>
+  validate?: (match: string) => Promise<boolean>
 }
 
 const props = withDefaults(defineProps<Props>(), {
   target: '_self',
 })
+
+const emit = defineEmits<{
+  'match-click': [event: MouseEvent, match: string]
+}>()
+
 const { text, regex, to, target, validate } = toRefs(props)
 
 // Ref to hold the parsed and validated text parts
-const parsedText = ref<{ text: string; linkId?: string; isLink: boolean }[]>([])
+const parsedText = ref<{ text: string; match?: string; matched: boolean }[]>([])
 
 // Watch the text and regex inputs for changes
 watch(
@@ -60,39 +71,39 @@ watch(
     const matches = [...text.value.matchAll(regex.value)]
 
     let lastIndex = 0
-    for (const match of matches) {
-      const matchIndex = match.index || 0
+    for (const matchObject of matches) {
+      const matchIndex = matchObject.index || 0
 
       // Add normal text before match
       if (matchIndex > lastIndex) {
         result.push({
           text: text.value.slice(lastIndex, matchIndex),
-          isLink: false,
+          matched: false,
         })
       }
 
-      const linkId = match[1]
+      const match = matchObject[1]
 
       // Push the match to the result
-      const index = result.push({ text: match[0], linkId, isLink: !validate?.value }) - 1
+      const index = result.push({ text: matchObject[0], match, matched: !validate?.value }) - 1
       if (validate.value) {
-        // Validate the linkId async if a validator is provided
-        validate.value(linkId).then((isValid: boolean) => {
+        // Validate the match async if a validator is provided
+        validate.value(match).then((isValid: boolean) => {
           if (isValid) {
-            // Update the isLink property if the linkId is valid
-            parsedText.value[index].isLink = true
-            result[index].isLink = true
+            // Update the matched property if the match is valid
+            parsedText.value[index].matched = true
+            result[index].matched = true
           }
         })
       }
 
       // Update lastIndex
-      lastIndex = matchIndex + match[0].length
+      lastIndex = matchIndex + matchObject[0].length
     }
 
     // Add any remaining text after the last match
     if (lastIndex < text.value.length) {
-      result.push({ text: text.value.slice(lastIndex), isLink: false })
+      result.push({ text: text.value.slice(lastIndex), matched: false })
     }
 
     parsedText.value = result
