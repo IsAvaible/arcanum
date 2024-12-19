f012fd8a8efbe953be088b29f7a5149df5cbbfd2
Test <Test>
2024-12-19 10:17:49 +0100

    Backend Fix

--- backend/controllers/fileuploadController.js
+++ backend/controllers/fileuploadController.js
@@ -6,6 +6,13 @@ const fs = require("fs");
 const env = require("dotenv").config();
 const router = express.Router();
 
+/**
+ * Validates the filename to ensure it does not contain a complete path.
+ *
+ * @param {Object} file - File object provided by Multer.
+ * @returns {string} The base filename if valid.
+ * @throws {Error} If the filename contains a full path.
+ */
 function checkFileName(file) {
   const fileName = path.basename(file.originalname);
   if (file.originalname !== fileName) {
@@ -14,27 +21,38 @@ function checkFileName(file) {
   return fileName;
 }
 
-// Check file type
+/**
+ * Validates the file type based on its extension and MIME type.
+ *
+ * @param {Object} file - File object provided by Multer.
+ * @param {function} cb - Callback function to signal success or failure.
+ * @returns {boolean|undefined} Returns `true` if file type is valid, otherwise calls the callback with an error message.
+ */
 function checkFileType(file, cb) {
-  // Allowed ext
+  // Allowed file extensions
   const filetypes = /jpeg|jpg|png|gif|txt|pdf|mp3|mp4|wav|doc/;
-  // Check ext
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
+  // Allowed MIME types
   const mimetypes =
-    /image\/jpeg|image\/jpg|image\/png|image\/gif|text\/plain|video\/mp4|application\/pdf|audio\/mpeg|audio\/wav|application\/msword/;
-  // Check mime
+    /image\/jpeg|image\/jpg|image\/png|image\/gif|text\/plain|application\/pdf|audio\/mpeg|audio\/wav|application\/msword/;
   const mimetype = mimetypes.test(file.mimetype);
 
   if (mimetype && extname) {
     console.log("File type check successful");
     return cb(null, true), extname;
   } else {
-    cb("Error: Images/Text Only!");
+    cb("Error: Images/Text Only!"); // Callback with an error for invalid types.
   }
 }
 
-// Function to scan file with Microsoft Azure
+/**
+ * Scans a file using Microsoft Azure Content Moderator.
+ *
+ * @param {string} file - The path to the file to be scanned.
+ * @returns {Promise<Object>} The response data from the Azure API.
+ * @throws {Error} If the scan fails or the API returns a non-200 status.
+ */
 async function scanFileWithAzure(file) {
   console.log(process.env.AZURE_API_KEY);
   const apiKey = process.env.AZURE_API_KEY;
@@ -44,6 +62,7 @@ async function scanFileWithAzure(file) {
   const fileName = path.basename(file);
 
   try {
+    // Send the file to the Azure Content Moderator API.
     const response = await axios.post(endpoint, fileStream, {
       headers: {
         "Content-Type": "application/octet-stream",
@@ -52,14 +71,15 @@ async function scanFileWithAzure(file) {
       },
     });
 
+    // Validate the response status.
     if (response.status !== 200) {
       throw new Error(`Error scanning file with Azure: ${response.statusText}`);
     }
 
-    return response.data;
+    return response.data; // Return the scan results.
   } catch (error) {
     console.error("Error scanning file with Azure:", error);
-    throw error;
+    throw error; // Re-throw the error for the caller to handle.
   }
 }
 
--- backend/controllers/nextCloudUploaderController.js
+++ backend/controllers/nextCloudUploaderController.js
@@ -1,162 +1,209 @@
-const crypto = require('crypto');
-const fs = require('fs');
+const crypto = require("crypto");
+const fs = require("fs");
 
 let nextcloudClient;
+
+// Initializes the connection to the Nextcloud client.
+// Uses WebDAV with credentials provided via environment variables.
 const nextcloudClientPromise = (async () => {
-    const { createClient } = await import('webdav');
-  
-    nextcloudClient = createClient(
-        process.env.NEXTCLOUD_URL,
-        {
-            username: process.env.NEXTCLOUD_USERNAME,
-            password: process.env.NEXTCLOUD_PASSWORD
-        }
-    );
-  })();
+  const { createClient } = await import("webdav");
 
-// Example function to list files in the root directory
+  nextcloudClient = createClient(process.env.NEXTCLOUD_URL, {
+    username: process.env.NEXTCLOUD_USERNAME,
+    password: process.env.NEXTCLOUD_PASSWORD,
+  });
+})();
+
+/**
+ * Lists files in the specified directory on Nextcloud.
+ *
+ * @param {string} remoteFilePath - The path to the remote directory on Nextcloud.
+ * @returns {Promise<Array>} Array of directory contents or `undefined` in case of an error.
+ */
 async function listFiles(remoteFilePath) {
-    try {
-        // Return the directory contents
-        return await nextcloudClient.getDirectoryContents(remoteFilePath);
-    } catch (error) {
-        console.error('Error listing files:', error);
-    }
+  try {
+    return await nextcloudClient.getDirectoryContents(remoteFilePath);
+  } catch (error) {
+    console.error("Error listing files:", error);
+  }
 }
 
-// list file in a specific folder and check if the file is already in the folder
+/**
+ * Lists files in a specific folder and checks if a given file exists.
+ *
+ * @param {string} remoteFolder - The path to the remote folder on Nextcloud.
+ * @param {string} fileName - The name of the file to check.
+ */
 async function listFilesSpec(remoteFolder, fileName) {
-    try {
-        const directoryItems = await nextcloudClient.getDirectoryContents(remoteFolder);
-        console.log(directoryItems);
-    } catch (error) {
-        console.error('Error listing files:', error);
-    }
+  try {
+    const directoryItems =
+      await nextcloudClient.getDirectoryContents(remoteFolder);
+    console.log(directoryItems);
+  } catch (error) {
+    console.error("Error listing files:", error);
+  }
 }
 
-// Example function to upload a file
+/**
+ * Uploads a file to a specific folder on Nextcloud, categorizing it based on its file type.
+ *
+ * @param {string} localFilePath - The local path to the file being uploaded.
+ * @param {string} remoteFilePath - The remote directory path on Nextcloud.
+ * @param {string} fileName - The name of the file being uploaded.
+ * @returns {string|number} The remote path of the uploaded file, or `-1` if the file type is unsupported.
+ */
 async function uploadFile(localFilePath, remoteFilePath, fileName) {
-    await nextcloudClientPromise;
-    const fileContent = fs.readFileSync(localFilePath);
-    let hashedFileContent = '';
+  await nextcloudClientPromise;
+  const fileContent = fs.readFileSync(localFilePath);
+  let hashedFileContent = "";
 
-    try{
-        hashedFileContent = crypto.createHash('sha256').update(fileContent).digest('hex');
-        console.log('Hashed file content:', hashedFileContent);
-    } catch (error) {   
-        console.error('Error hashing file content:', error);
-    }
-    
-    const folder = ["Audio/", "Bilder/", "Text/", "Videos/"];
-    const fileExtension = fileName.split('.').pop().toLowerCase();
-    let folderPath = '';
+  // Generate a hash for the file content for uniqueness.
+  try {
+    hashedFileContent = crypto
+      .createHash("sha256")
+      .update(fileContent)
+      .digest("hex");
+    console.log("Hashed file content:", hashedFileContent);
+  } catch (error) {
+    console.error("Error hashing file content:", error);
+  }
 
-    console.log("Fieltype: " + fileExtension);
-    switch (fileExtension) {
-        case 'mp3':
-        case 'wav':
-            folderPath = folder[0]; // Audio/
-            break;
-        case 'jpg':
-        case 'jpeg':
-        case 'png':
-        case 'gif':
-            folderPath = folder[1]; // Bilder/
-            break;
-        case 'txt':
-        case 'doc':
-        case 'pdf':
-            folderPath = folder[2]; // Text/
-            break;
-        case 'mp4':
-            folderPath = folder[3]; // Videos/
-            break;
-    }
-    if(folderPath === ''){
-        console.log('File type not found');
-        return -1;
-    }
-    
-    try {
-        const directoryItems = await listFiles(remoteFilePath + folderPath);
-        remoteFilePath = remoteFilePath + folderPath + hashedFileContent.toString() + '.' + fileExtension;
-        
-        if(directoryItems !== undefined){
-            for (const item of directoryItems) {
-                    //console.log('Item:', item.basename + " == " + hashedFileContent.toString()+ "." + fileExtension);
-                    if (item.basename === hashedFileContent.toString() + "." + fileExtension) {
-                        console.log('File already exists in the folder, skipping upload');
-                        return remoteFilePath;
-                    }
-                }
-            } else {
-                console.log('Can not list Folder content');
-            }
-    
-        
-        await nextcloudClient.putFileContents(remoteFilePath, fileContent);
-        console.log('File uploaded successfully');
+  // Define folder categories based on file type.
+  const folder = ["Audio/", "Images/", "Text/", "Videos/"];
+  const fileExtension = fileName.split(".").pop().toLowerCase();
+  let folderPath = "";
 
-    } catch (error) {
-        console.error('Error uploading file:', error);
-    } finally {
-        
-        fs.unlink(localFilePath, (err) => {
-            if (err) {
-              console.error('Error deleting file:', err);
-            } else {
-              console.log('File deleted successfully');
-            }
-          });
+  console.log("File type: " + fileExtension);
+  switch (fileExtension) {
+    case "mp3":
+    case "wav":
+      folderPath = folder[0]; // Audio/
+      break;
+    case "jpg":
+    case "jpeg":
+    case "png":
+    case "gif":
+      folderPath = folder[1]; // Images/
+      break;
+    case "txt":
+    case "doc":
+    case "pdf":
+      folderPath = folder[2]; // Text/
+      break;
+    case "mp4":
+      folderPath = folder[3]; // Videos/
+      break;
+  }
+
+  if (folderPath === "") {
+    console.log("File type not supported");
+    return -1;
+  }
+
+  try {
+    // Check if the file already exists in the target folder.
+    const directoryItems = await listFiles(remoteFilePath + folderPath);
+    remoteFilePath =
+      remoteFilePath +
+      folderPath +
+      hashedFileContent.toString() +
+      "." +
+      fileExtension;
+
+    if (directoryItems !== undefined) {
+      for (const item of directoryItems) {
+        if (
+          item.basename ===
+          hashedFileContent.toString() + "." + fileExtension
+        ) {
+          console.log("File already exists, skipping upload");
+          return remoteFilePath;
+        }
+      }
+    } else {
+      console.log("Cannot list folder content");
     }
 
-    return remoteFilePath;
+    // Upload the file to Nextcloud.
+    await nextcloudClient.putFileContents(remoteFilePath, fileContent);
+    console.log("File uploaded successfully");
+  } catch (error) {
+    console.error("Error uploading file:", error);
+  } finally {
+    // Delete the local file after uploading.
+    fs.unlink(localFilePath, (err) => {
+      if (err) {
+        console.error("Error deleting file:", err);
+      } else {
+        console.log("File deleted successfully");
+      }
+    });
+  }
+
+  return remoteFilePath;
 }
 
-// Example function to download a file
+/**
+ * Downloads a file from Nextcloud and saves it locally.
+ *
+ * @param {string} remoteFilePath - The remote path to the file on Nextcloud.
+ * @param {string} localFilePath - The local path where the file will be saved.
+ */
 async function downloadFile(remoteFilePath, localFilePath) {
-
-    try {
-        const fileContent = await nextcloudClient.getFileContents(remoteFilePath);
-        fs.writeFileSync(localFilePath, fileContent);
-        console.log('File downloaded successfully');
-    } catch (error) {
-        console.error('Error downloading file:', error);
-    }
+  try {
+    const fileContent = await nextcloudClient.getFileContents(remoteFilePath);
+    fs.writeFileSync(localFilePath, fileContent);
+    console.log("File downloaded successfully");
+  } catch (error) {
+    console.error("Error downloading file:", error);
+  }
 }
 
-
+/**
+ * Downloads a file from Nextcloud and returns its content.
+ *
+ * @param {string} remoteFilePath - The remote path to the file on Nextcloud.
+ * @returns {Buffer} The content of the downloaded file.
+ */
 async function downloadFileAndReturn(remoteFilePath) {
-
-    try {
-        const fileContent = await nextcloudClient.getFileContents(remoteFilePath);
-        console.log('File downloaded successfully');
-        return fileContent
-    } catch (error) {
-        console.error('Error downloading file:', error);
-    }
-
+  try {
+    const fileContent = await nextcloudClient.getFileContents(remoteFilePath);
+    console.log("File downloaded successfully");
+    return fileContent;
+  } catch (error) {
+    console.error("Error downloading file:", error);
+  }
 }
 
+/**
+ * Creates a readable stream for a file on Nextcloud.
+ *
+ * @param {string} remoteFilePath - The remote path to the file on Nextcloud.
+ * @returns {Stream} A readable stream representing the file.
+ */
 async function streamFile(remoteFilePath) {
-    return nextcloudClient.createReadStream(remoteFilePath);
+  return nextcloudClient.createReadStream(remoteFilePath);
 }
 
+/**
+ * Deletes a file from Nextcloud.
+ *
+ * @param {string} remoteFilePath - The remote path to the file on Nextcloud.
+ */
 async function deleteFile(remoteFilePath) {
-    try {
-        await nextcloudClient.deleteFile(remoteFilePath);
-        console.log('File deleted successfully');
-    } catch (error) {
-        console.error('Error deleting file:', error);
-    }
+  try {
+    await nextcloudClient.deleteFile(remoteFilePath);
+    console.log("File deleted successfully");
+  } catch (error) {
+    console.error("Error deleting file:", error);
+  }
 }
-
 
 module.exports = {
-    downloadFileAndReturn,
-    streamFile,
-    listFiles,
-    uploadFile,
-    downloadFile,
-    deleteFile
+  downloadFileAndReturn,
+  streamFile,
+  listFiles,
+  uploadFile,
+  downloadFile,
+  deleteFile,
 };
