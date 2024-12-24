df32c3c10c9074bcd4f5d72c34403fa96d8772b3
Test <Test>
2024-12-24 15:32:07 +0100

    Comments added

--- llm_backend/video.py
+++ llm_backend/video.py
@@ -5,21 +5,32 @@ import subprocess
 import cv2
 
 from app import app
-from image import encode_image, image_to_openai
+from image import encode_image, image_to_openai, video_openai
+
+split_secs = 100
 
 
-def cut_video_segments(input_file, filehash, segment_duration=100):
+"""
+OpenAI has a limit of 50 pictures each request. If we have a 10 min video that would be too less information.
+Because of that we need to split longer videos into multiple segments.
+If a video is over 100 seconds we are splitting it into multiple segments. Each segment will be approxametly 100 seconds long.
+From these segments we are getting 50 frames (1 Frame every 2 Seconds)
+
+For cutting the videos we are using ffmpeg which is the most used software for video processing.
+"""
+def cut_video_segments(input_file, filehash, segment_duration=split_secs):
+
+    # define ouput
     output_path = os.path.join(
         app.root_path, os.path.join(f"temp/{filehash}/", "video_segments")
     )
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-
     output_pattern = os.path.join(output_path, "videos_%04d.mp4")
 
     command = [
         "ffmpeg",
-        "-v", "quiet",
+        "-v", "quiet", # less logs
         "-i", input_file,  # Input file
         "-c", "copy",  # Copy codec to avoid re-encoding
         "-map", "0",  # Map all streams
@@ -29,6 +40,7 @@ def cut_video_segments(input_file, filehash, segment_duration=100):
         output_pattern  # Output file pattern
     ]
 
+    #run ffmpeg process
     try:
         subprocess.run(command, check=True)
         return output_path
@@ -37,45 +49,51 @@ def cut_video_segments(input_file, filehash, segment_duration=100):
 
 
 def extract_data_from_video(video_path, filehash):
-    # Erstelle den Ausgabeordner, falls er nicht existiert
+    # define ouput
     single_video = video_path
-
     frames_path = os.path.join(
         app.root_path, os.path.join(f"temp/{filehash}/", "frames")
     )
-
     if not os.path.exists(frames_path):
         os.makedirs(frames_path)
 
-    # Berechne die Skalierung
+    # Calculate scale
     cam = cv2.VideoCapture(single_video)
     fps = cam.get(cv2.CAP_PROP_FPS)
     total_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration = total_frames / fps  # Dauer des Videos in Sekunden
+    # get duration of video to check if we need splitting
+    duration = total_frames / fps
 
-    if (duration > 120):
+
+    # get segments if video longer 100 secs
+    if (duration > split_secs):
         segments_path = cut_video_segments(single_video, filehash)
         segments = get_all_video_segments_in_dir(segments_path)
     else:
         segments = [single_video]
 
-    i = 1
-    for video in segments:
 
-        vf_filter = "fps=0.2 ,scale=320:-1"
+    # iterate over segments
+    for i, video in segments:
+
+        # Scale video down to width of 320 and the corresponding height based on the aspect ratio
+        # get one frame each 2 seconds
+        vf_filter = "fps=1/2 ,scale=320:-1"
 
         # FFmpeg-Befehl ausführen
         output_pattern = os.path.join(frames_path, "frame_%04d.jpg")
+
+        # count the frames that have been already saved
         counter = sum(1 for item in os.listdir(frames_path) if os.path.isfile(os.path.join(frames_path, item)))
 
         command = [
             "ffmpeg",
-            "-v", "quiet",
-            "-y",
-            "-i", video,
-            "-vf", vf_filter,
-            "-start_number", str(counter),
-            output_pattern
+            "-v", "quiet", # less logs
+            "-y", #override file if exists
+            "-i", video, # input video
+            "-vf", vf_filter, # apply filter
+            "-start_number", str(counter), # set start counter for the frames
+            output_pattern # define ouput pattern
         ]
 
         try:
@@ -85,6 +103,8 @@ def extract_data_from_video(video_path, filehash):
         except subprocess.CalledProcessError as e:
             print(f"Error FFMPEG (Frame Extraction): {e}")
 
+
+    # we also need to transcribe the audio in the video
     audio_path = os.path.join(
         app.root_path, os.path.join(f"temp/{filehash}/", "audio")
     )
@@ -94,11 +114,11 @@ def extract_data_from_video(video_path, filehash):
     audio_output = os.path.join(audio_path, "audio.mp3")
     command = [
         "ffmpeg",
-        "-v", "quiet",
-        "-y",
-        "-i", single_video,
+        "-v", "quiet", #less logs
+        "-y", # override file
+        "-i", single_video, # set input file
         "-vn",
-        audio_output
+        audio_output #define output
     ]
 
     try:
@@ -107,28 +127,37 @@ def extract_data_from_video(video_path, filehash):
     except subprocess.CalledProcessError as e:
         print(f"Error FFMPEG (Audio Extraction): {e}")
 
+    # return path of frames and audio file
     return frames_path, audio_output
 
 
 # todo bereits zusammengefasste frames hinzufügen und transcription
 def process_segments(frames, result_dict, transcription):
-    print(len(frames))
-    frame_segments = math.floor(len(frames) / 49)
-    print(f"segments: {frame_segments}")
 
+    print(f"Frame Count:{str(len(frames))}")
+    # calculate how many rounds we need to analyze the frames
+    # here we are dividing by 49 and rounding that value up
+    frame_segments = math.floor(len(frames) / 49)
+    print(f"Segment Count: {frame_segments}")
     video_summary = ""
+
     if frame_segments > 0:
+        # prompt_dict will include all frames that we need to analyze
         prompt_dict = []
+
+        # iterate over all segments (frame_count/50)
         for i in range(0, frame_segments):
+            print("Analyzing Segment " + str(i) + " / " + str(frame_segments))
             prompt_dict.clear()
             prompt_dict = [
                 {
                     "type": "text",
-                    "text": f"Here is part {i} of {frame_segments}. What are all frames showing, be as detailed as possible but please combine everything in a normal text"
+                    "text": f"Here is part {str(i)} of {str(frame_segments)}. What are all frames showing, be as detailed as possible but please combine everything in a normal text"
                 }
             ]
-            print("range - " + str(0 + (50 * i)) + " - " + str(49 * (i + 1)))
+
             for j in range(0 + (50 * i), 49 * (i + 1)):
+                # images need to be base64 encoded otherwise Azure OpenAI wont understand them
                 encoding = encode_image(frames[j])
                 base64_image = {
                     "type": "image_url",
@@ -138,11 +167,9 @@ def process_segments(frames, result_dict, transcription):
                     }
                 }
                 prompt_dict.append(base64_image)
-            video_summary = image_to_openai(prompt_dict)
-            print(video_summary)
-            # append
-            video_summary = video_summary + " "
-        single_dict = result_dict
+            sum_part = image_to_openai(prompt_dict)
+            print(f"Segment {i} - {sum_part}")
+            video_summary = sum_part + " "
     else:
         prompt_dict = []
         prompt_dict.clear()
@@ -164,12 +191,16 @@ def process_segments(frames, result_dict, transcription):
             prompt_dict.append(base64_image)
         video_summary = image_to_openai(prompt_dict)
 
-
-
+    print(f"Complete Summary - {video_summary}")
+    #result = video_openai(video_summary, transcription)
+    result = video_summary
+    print(f"Complete Summary after LLM - {result}")
+    result_dict["video_summary"] = result
     single_dict = result_dict
     return single_dict
 
 
+# method to get all frames in a directory
 def get_all_frames_in_dir(path):
     f = []
     for (dirpath, dirnames, filenames) in os.walk(path):
@@ -177,7 +208,7 @@ def get_all_frames_in_dir(path):
             f.append(os.path.join(dirpath, file))
     return f
 
-
+# method to get all video segments in a directory
 def get_all_video_segments_in_dir(path):
     f = []
     for (dirpath, dirnames, filenames) in os.walk(path):
