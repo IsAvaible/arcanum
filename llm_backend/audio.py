026899668790d0de72cee75e8d95ef5de1e771e6
Test <Test>
2024-12-24 16:34:01 +0100

    Comments added

--- llm_backend/audio.py
+++ llm_backend/audio.py
@@ -1,13 +1,11 @@
 from pydub import AudioSegment
 
 
-"""
+'''
 Splits audio into multiple files
-
-Due to Azure´s limit of 25 Mb for each audio file we need to split audio files into multiple chunks if they exceed this limit
-This method also includes a little overlap so cutted words during a split can be recognized
-
-"""
+    Due to Azure´s limit of 25 Mb for each audio file we need to split audio files into multiple chunks if they exceed this limit
+    This method also includes a little overlap so cutted words during a split can be recognized
+'''
 def split_audio_with_overlap(file_path, segment_length_ms=300000, overlap_ms=10000):
     audio = AudioSegment.from_file(file_path)
     segments = []
