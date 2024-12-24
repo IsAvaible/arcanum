7a3238adc4094a910b656d94673321294c7ac5d5
Test <Test>
2024-12-24 16:34:14 +0100

    Comments added

--- llm_backend/hash.py
+++ llm_backend/hash.py
@@ -1,17 +1,17 @@
 import hashlib
 
-
+# generate SHA256 hash of a file
 def generate_sha256_hash(file_path):
     sha256_hash = hashlib.sha256()
     try:
         with open(file_path, "rb") as file:
-            # Datei in Blöcken lesen, um große Dateien zu unterstützen
+            # read file in blocks
             for block in iter(lambda: file.read(4096), b""):
                 sha256_hash.update(block)
         return sha256_hash.hexdigest()
     except FileNotFoundError:
-        print(f"Die Datei '{file_path}' wurde nicht gefunden.")
+        print(f"File '{file_path}' not found.")
         return None
     except Exception as e:
-        print(f"Ein Fehler ist aufgetreten: {e}")
+        print(f"Error occured: {e}")
         return None
