12df250c9e4aa1739b147d1ece623fa6aaca72bd
justins03 <justinschulz2003@gmail.com>
2025-01-08 19:08:19 +0100

    feat(llm_chat): enhanced retrieval, system msg and added translation of doc ids to file/case ids

--- llm_backend/generate.py
+++ llm_backend/generate.py
@@ -7,6 +7,7 @@ from prompts import get_system_prompt
 from upload import upload_file_method_production
 from vectorstore import QdrantVectorstore
 import json
+import re
 
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -148,9 +149,57 @@ def vector_db_save_cases_backend(request):
 
     return "Cases Saved Successfully", 200
 
+def transform_to_standalone_question(chat_history):
+    system_prompt = """
+        You are an AI assistant. Your task is to transform the latest human message in the chat history into a standalone question that can be understood without the previous context and in the same language it is written in.
+        
+        For example, if the AI says "I am feeling tired" and the human says "Why", the question "Why" wouldn't be understandable without the context. It should be transformed to "Why are you feeling tired?".
+        
+        Here is the chat history:
+        {chat_history}
+        
+        Please provide the standalone question based on the latest human message. DO NOT include anything else.
+    """
+
+    llm = AzureChatOpenAI(
+        azure_endpoint=AZURE_ENDPOINT,
+        azure_deployment=AZURE_DEPLOYMENT_GPT,
+        openai_api_version=OPENAI_API_VERSION,
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        streaming=False,
+    )
+
+    prompt = system_prompt.format(chat_history=chat_history)
+    response = llm([{"role": "system", "content": prompt}]).content
+
+    return response
+
+def replace_doc_number(input_string, replacement_dict):
+    # Define the regex pattern to match [doc_number:number]
+    pattern = r'\[doc_number:(\d+)\]'
+    
+    # Function to determine the replacement string based on the number
+    def replacement_function(match):
+        number = match.group(1)
+        return f"[{replacement_dict[int(number)]}]"
+    
+    # Use re.sub with a function to replace the pattern with the appropriate string
+    result = re.sub(pattern, replacement_function, input_string)
+    
+    return result
+
+def transform_messages_for_llm(messages):
+    return [{'role': message['role'], 'content': message['content']} for message in messages[:-1]]
+
+
 def ask_question(request):
     json_str = request.get_json(force=True)
     messages = json_str
+
+    massages_only_role_content = transform_messages_for_llm(messages)
 
     # Extract the latest user message
     latest_user_message = next(
@@ -162,8 +211,10 @@ def ask_question(request):
 
     vectorstore = QdrantVectorstore()
 
-    relevant_vectors = vectorstore.search_from_query(latest_user_message)
+    standalone_question = transform_to_standalone_question(json.dumps(massages_only_role_content))
+    relevant_vectors = vectorstore.search_from_query(standalone_question)
 
+    replacement_dict = {}
     context = ""
     doc_number = 1
     for vector in relevant_vectors:
@@ -173,8 +224,10 @@ def ask_question(request):
 
         if metadata["inserttype"] == "case":
             text = f"Case content [doc_number:{doc_number}]:\n" + text
+            replacement_dict[doc_number] = "case:"+str(metadata["case_id"])
         elif metadata["inserttype"] == "attachment-chunk":
             text = metadata["filename"] + f" [doc_number:{doc_number}]:\n" + text
+            replacement_dict[doc_number] = "file:"+str(metadata["file_id"])
 
         doc_number += 1
 
@@ -182,7 +235,7 @@ def ask_question(request):
 
         context += text
 
-    prompt = f"CONTEXT: {context}\n\nQUERY: {latest_user_message}"
+    user_query = f"{latest_user_message}"
 
     llm = AzureChatOpenAI(
         azure_endpoint=AZURE_ENDPOINT,
@@ -197,11 +250,14 @@ def ask_question(request):
 
     # Use the history from json_str
     messages = [
-        {"role": "system", "content": "You are a helpful assistant. Always cite the context in your response by writing '[doc_number:number]' and replacing number with the actual number of the document and doc_number staying the same for the program to corretly identfy your citing."},
-        *messages,
-        {"role": "user", "content": prompt},
+        {"role": "system", "content": "You are a helpful assistant. Answer user questions based only on the provided context, which includes cases of problems with machines and their solutions, manuals, notes, transcribed and textualized video, audio, and image content, and other related documents. Always respond in the same language as the user. Cite the context in your response by writing '[doc_number:number]' and replacing number with the actual number of the document and doc_number staying the same for the program to correctly identify your citing. Ignore if previous responses used different citing formats. Just stick to the describe citing format."},
+        *massages_only_role_content,
+        {"role": "system", "content": f"CONTEXT for next query: {context}"},
+        {"role": "user", "content": user_query},
     ]
     response = llm(messages).content
+
+    response = replace_doc_number(response, replacement_dict)
 
     return jsonify({"response": response}), 200
 
